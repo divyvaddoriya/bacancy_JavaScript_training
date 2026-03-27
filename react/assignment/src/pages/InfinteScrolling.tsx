6ccddfa044d98b4d00b2c2c51f71479a75af6769
divy <vaddoriyadivy12@gmail.com>
2026-03-27 10:43:15 +0530

    "infinite scrolling added"

--- react/assignment/src/pages/InfinteScrolling.tsx
+++ react/assignment/src/pages/InfinteScrolling.tsx
@@ -1,32 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import Product from '../component/Product'
+import React, { useEffect, useRef, useState } from "react";
+import Product from "../component/Product";
 
 const InfinteScrolling = () => {
+  const [products, setProducts] = useState<any[]>([]);
+  const [skip, setSkip] = useState(0);
+  const [loading , setLoading] = useState(true);
+  const last = useRef<HTMLDivElement| null>(null);
 
-    const [products , setProducts] = useState<any[]>([])
- 
-    useEffect(() => {
-        async function fetchData() {
-            const data = await fetch("https://dummyjson.com/products")
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        
+      const currentLastElement = entries[0];
+        
+        if ( !loading && currentLastElement.isIntersecting) {
+          console.log("hi");
 
-            const result = await data.json()
+          setSkip((prev) => prev + 20);
+          observer.unobserve(currentLastElement.target);
+        }
+        
+      },
+      {
+        threshold: 0.5,
+        rootMargin: "10px",
+      },
+    );
 
-            setProducts(result.products)
-        }    
-        fetchData()
-} , [])
+    if (last.current)  observer.observe(last.current);
 
+  }, [loading]);
 
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true)
+      const data = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${skip}`,
+      );
+
+      const result = await data.json();
+      setProducts((prev) => [...prev, ...result.products]);
+      setLoading(false)
+    }
+
+    fetchData();
+  }, [skip]);
 
   return (
-    <div style={{display: "flex" , justifyContent: "space-between" , flexWrap: "wrap", gap:"15px" }}>
-        
-    {products.map((product) => (
-        <Product product = {product}/>
-    ))}
-
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        gap: "15px",
+      }}
+    >
+      {products.map((product) => (
+        <Product product={product} />
+      ))}
+      <div ref={last} style={{ width: "100px", height: "100px" }}></div>
+    {loading && <div>
+      i am loading
+     </div> }
     </div>
-  )
-}
+  );
+};
 
-export default InfinteScrolling
+export default InfinteScrolling;
